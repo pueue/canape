f92dd44a3c2da10feadfcebc08d0170eae43011f
pueue <pueuepueue@gmail.com>
2016-05-24 23:20:22 +0900

    Code 모델 생성

--- postage/models.py
+++ postage/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 class Postage(models.Model):
+	maker = models.ForeignKey(
+				settings.AUTH_USER_MODEL,
+				on_delete=models.CASCADE,
+	)
 	title = models.CharField(
 				max_length=200,
 	)
@@ -13,3 +18,18 @@ class Postage(models.Model):
 	quantity = models.IntegerField()
 	created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+class Code(models.Model):
+	gainer = models.ForeignKey(
+				settings.AUTH_USER_MODEL,
+				on_delete=models.CASCADE,
+	)
+	postage = models.ForeignKey(
+				Postage,
+				on_delete=models.CASCADE,
+	)
+	code = models.CharField(
+				max_length=16,
+	)
+	serial = models.IntegerField()
+	created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
