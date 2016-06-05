aa948fe4cf5ec7e507fa818feb2905243c33b86d
pueue <pueuepueue@gmail.com>
2016-06-06 01:02:06 +0900

    Postage 모델 quantity필드 기본값추가 및 form validation 추가

--- postage/forms.py
+++ postage/forms.py
@@ -21,18 +21,36 @@ class PostageForm(forms.ModelForm):
 			'class': 'form-control',
 		}))
 	is_transferable = forms.BooleanField(label="is_transferable",
+		required=False,
 		widget=forms.CheckboxInput(attrs={
 			'class': 'form-control',
 		}))
 	is_limit = forms.BooleanField(label="is_limit",
+		required=False,
 		widget=forms.CheckboxInput(attrs={
 			'class': 'form-control',
 		}))
 	quantity = forms.IntegerField(label="quantity",
+		required=False,
 		widget=forms.NumberInput(attrs={
 			'class': 'form-control',
-		}))
+		}),
+		help_text = "Only required if 'is_limit' is selected.")
 
 	class Meta:
 		model = Postage
 		fields = ("title", "image", "description", "is_transferable", "is_limit", "quantity")
+
+	def clean(self):
+		if self.cleaned_data.get('is_limit'):
+			self.fields_required(['quantity'])
+		else:
+			self.cleaned_data['quantity'] = ''
+		return self.cleaned_data
+
+	def fields_required(self, fields):
+		"""Used for conditionally marking fields as required."""
+		for field in fields:
+			if not self.cleaned_data.get(field, ''):
+				msg = forms.ValidationError("This field is required.")
+				self.add_error(field, msg)
--- postage/models.py
+++ postage/models.py
@@ -15,7 +15,7 @@ class Postage(models.Model):
 	description = models.TextField()
 	is_transferable = models.BooleanField(default=False)
 	is_limit = models.BooleanField(default=False)
-	quantity = models.IntegerField()
+	quantity = models.IntegerField(default=0)
 	created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
